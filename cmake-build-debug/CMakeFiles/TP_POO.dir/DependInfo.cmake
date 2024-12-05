c7172b919aec3dae6af085ab9e98381017ec2fc9
JPBO04 <167642377+JPBO04@users.noreply.github.com>
2024-12-05 12:48:43 +0000

    teste Buffer

---
+++ cmake-build-debug/CMakeFiles/TP_POO.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/jpbol/Documents/Universidade/2º ANO/3º semestre/POO/TP_POO/Classes/Buffer.cpp" "CMakeFiles/TP_POO.dir/Classes/Buffer.cpp.obj" "gcc" "CMakeFiles/TP_POO.dir/Classes/Buffer.cpp.obj.d"
+  "C:/Users/jpbol/Documents/Universidade/2º ANO/3º semestre/POO/TP_POO/Classes/Caravanas.cpp" "CMakeFiles/TP_POO.dir/Classes/Caravanas.cpp.obj" "gcc" "CMakeFiles/TP_POO.dir/Classes/Caravanas.cpp.obj.d"
+  "C:/Users/jpbol/Documents/Universidade/2º ANO/3º semestre/POO/TP_POO/Classes/Cidades.cpp" "CMakeFiles/TP_POO.dir/Classes/Cidades.cpp.obj" "gcc" "CMakeFiles/TP_POO.dir/Classes/Cidades.cpp.obj.d"
+  "C:/Users/jpbol/Documents/Universidade/2º ANO/3º semestre/POO/TP_POO/Classes/Deserto.cpp" "CMakeFiles/TP_POO.dir/Classes/Deserto.cpp.obj" "gcc" "CMakeFiles/TP_POO.dir/Classes/Deserto.cpp.obj.d"
+  "C:/Users/jpbol/Documents/Universidade/2º ANO/3º semestre/POO/TP_POO/Classes/Itens.cpp" "CMakeFiles/TP_POO.dir/Classes/Itens.cpp.obj" "gcc" "CMakeFiles/TP_POO.dir/Classes/Itens.cpp.obj.d"
+  "C:/Users/jpbol/Documents/Universidade/2º ANO/3º semestre/POO/TP_POO/main.cpp" "CMakeFiles/TP_POO.dir/main.cpp.obj" "gcc" "CMakeFiles/TP_POO.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/TP_POO.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP_POO.dir/Classes/Buffer.cpp.obj"
+  "CMakeFiles/TP_POO.dir/Classes/Buffer.cpp.obj.d"
+  "CMakeFiles/TP_POO.dir/Classes/Caravanas.cpp.obj"
+  "CMakeFiles/TP_POO.dir/Classes/Caravanas.cpp.obj.d"
+  "CMakeFiles/TP_POO.dir/Classes/Cidades.cpp.obj"
+  "CMakeFiles/TP_POO.dir/Classes/Cidades.cpp.obj.d"
+  "CMakeFiles/TP_POO.dir/Classes/Deserto.cpp.obj"
+  "CMakeFiles/TP_POO.dir/Classes/Deserto.cpp.obj.d"
+  "CMakeFiles/TP_POO.dir/Classes/Itens.cpp.obj"
+  "CMakeFiles/TP_POO.dir/Classes/Itens.cpp.obj.d"
+  "CMakeFiles/TP_POO.dir/main.cpp.obj"
+  "CMakeFiles/TP_POO.dir/main.cpp.obj.d"
+  "TP_POO.exe"
+  "TP_POO.exe.manifest"
+  "TP_POO.pdb"
+  "libTP_POO.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP_POO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
