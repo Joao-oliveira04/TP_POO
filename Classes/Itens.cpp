09d3c3bd08eda6de3ab289892cc322dfb563e536
DavidLucasLoureiro <davidlucasloureiro@gmail.com>
2024-12-28 20:37:55 +0000

    fix erro itens

--- Classes/Itens.cpp
+++ Classes/Itens.cpp
@@ -1,5 +1,45 @@
-//
-// Created by david on 14/11/2024.
-//
-
 #include "Itens.h"
+#include "Caravanas.h"
+
+void CaixaDePandora::aplicarEfeito(Caravana* caravana) {
+    if (caravana) {
+        int perda = caravana->getTripulantes() * 0.2; // 20% da tripulação
+        caravana->setTripulantes(caravana->getTripulantes() - perda);
+        std::cout << "Caixa de Pandora: " << perda << " tripulantes mortos!\n";
+    }
+}
+
+void ArcaDoTesouro::aplicarEfeito(Caravana* caravana) {
+    if (caravana) {
+        int moedasAtuais = caravana->getMoedasJogador();
+        int incremento = moedasAtuais * 0.1;
+        caravana->setMoedasJogador(moedasAtuais + incremento);
+        std::cout << "Arca do Tesouro: Adicionou " << incremento
+                  << " moedas ao jogador! Total: " << caravana->getMoedasJogador() << " moedas.\n";
+    }
+}
+
+void Jaula::aplicarEfeito(Caravana* caravana) {
+    if (caravana) {
+        int capacidadeLivre = caravana->getMaxTripulantes() - caravana->getTripulantes();
+        int aumento = std::min(5, capacidadeLivre);
+        caravana->setTripulantes(caravana->getTripulantes() + aumento);
+        std::cout << "Jaula: Libertou prisioneiros! Tripulantes aumentados em " << aumento << "!\n";
+    }
+}
+
+void Mina::aplicarEfeito(Caravana* caravana) {
+    if (caravana) {
+        caravana->destruir();
+        std::cout << "Mina: Caravana destruída!\n";
+    }
+}
+
+void Surpresa::aplicarEfeito(Caravana* caravana) {
+    if (caravana) {
+        int novaPosX = rand() % 10;
+        int novaPosY = rand() % 10;
+        caravana->setPos(novaPosX, novaPosY);
+        std::cout << "Surpresa: Caravana teleporta para (" << novaPosX << ", " << novaPosY << ")!\n";
+    }
+}
--- Classes/Itens.h
+++ Classes/Itens.h
@@ -1,13 +1,11 @@
-//
-// Created by david on 14/11/2024.
-//
 
 #ifndef TP_POO_ITENS_H
 #define TP_POO_ITENS_H
 
-#include "Caravanas.h"
 #include <iostream>
 #include <string>
+
+class Caravana; // Forward declaration
 
 /*
  * Caixa de Pandora - mata 20% da tripulação FEITO
@@ -19,101 +17,47 @@
 class Itens {
 protected:
     std::string tipo;
-    int posX, posY; // posicao das coisas no deserto
+    int posX, posY;
 
 public:
-    Itens( const std::string t, int X, int Y) : tipo(t), posX(X), posY(Y) {}
+    Itens(const std::string t, int X, int Y) : tipo(t), posX(X), posY(Y) {}
     virtual ~Itens() {}
 
-    virtual void aplicarEfeito(Caravana* caravana) =  0; // Um metodo para aplicar o efeito
+    virtual void aplicarEfeito(Caravana* caravana) = 0;
 
     int getPosX() const { return posX; }
     int getPosY() const { return posY; }
     std::string getTipo() const { return tipo; }
 };
 
-// Subclasse: Caixa de Pandora
-class CaixaDePandora : public Itens
-{
+class CaixaDePandora : public Itens {
 public:
-    CaixaDePandora(int x,int y) : Itens("caixa de Pandora",x,y) {}
-    void aplicarEfeito(Caravana* caravana) override
-    {
-        if (caravana)
-        {
-            int perda = caravana->getTripulantes() * 0.2; // 20% da tripulacao
-            caravana->setTripulantes(caravana->getTripulantes() - perda);
-            std::cout << "caixa de Pandora: " << perda << " tripulantes mortos!\n";
-        }
-    }
+    CaixaDePandora(int x, int y) : Itens("caixa de Pandora", x, y) {}
+    void aplicarEfeito(Caravana* caravana) override;
 };
 
-// Subclasse: Arco do Tesouro
-class ArcaDoTesouro : public Itens
-{
+class ArcaDoTesouro : public Itens {
 public:
-    ArcaDoTesouro(int x,int y) : Itens("arca do Tesouro",x,y) {}
-    void aplicarEfeito(Caravana* caravana) override
-    {
-        if (caravana)
-        {
-            // Obtem a quantidade de moedas atual do utilizador
-            int moedasAtuais = caravana->getTripulantes(); // assumindo que a caravana sabe o valor do jogador
-            int incremento = moedasAtuais * 0.1; // calcula os tais + 10% das moedas
-            caravana->setMoedasJogador(moedasAtuais + incremento); // Atualiza as moedas do jogador
-
-            std::cout << "Arca do Tesouro: Adicionou " << incremento
-                      << " moedas ao jogador! Total: "
-                      << caravana->getMoedasJogador() << " moedas.\n";
-        }
-    }
+    ArcaDoTesouro(int x, int y) : Itens("arca do Tesouro", x, y) {}
+    void aplicarEfeito(Caravana* caravana) override;
 };
 
-// Subclasse: Jaula
 class Jaula : public Itens {
 public:
     Jaula(int x, int y) : Itens("Jaula", x, y) {}
-    void aplicarEfeito(Caravana* caravana) override {
-        if (caravana) {
-            int capacidadeLivre = caravana->getMaxTripulantes() - caravana->getTripulantes();
-            int aumento = std::min(5, capacidadeLivre); // Aumenta até 5 tripulantes ou o máximo permitido
-            caravana->setTripulantes(caravana->getTripulantes() + aumento);
-            std::cout << "Jaula: Libertou prisioneiros! Tripulantes aumentados em " << aumento << "!\n";
-        }
-    }
+    void aplicarEfeito(Caravana* caravana) override;
 };
 
-// Subclasse: Mina
-class Mina : public Itens
-{
+class Mina : public Itens {
 public:
-    Mina (int x,int y) : Itens("Mina",x,y) {}
-    void aplicarEfeito(Caravana* caravana) override
-    {
-        if (caravana)
-        {
-            caravana->destruir(); // a caravana fica destruida
-            std::cout << "Mina: Caravana destruída!\n";
-        }
-    }
+    Mina(int x, int y) : Itens("Mina", x, y) {}
+    void aplicarEfeito(Caravana* caravana) override;
 };
 
-//Subclasse: Surpresa
-class Surpresa : public Itens
-{
+class Surpresa : public Itens {
 public:
-    Surpresa (int x,int y) : Itens("Surpresa",x,y) {}
-    void aplicarEfeito(Caravana* caravana) override
-    {
-        if (caravana)
-        {
-            // Define o efeito surpresa - exemplo: teletransportar a caravana ATENCAO FOI O CHAT....
-            int novaPosX = rand() % 10; // Exemplo de teletransporte
-            int novaPosY = rand() % 10;
-            caravana->setPos(novaPosX, novaPosY);
-            std::cout << "Surpresa: Caravana teleporta para (" << novaPosX << ", " << novaPosY << ")!\n";
-        }
-    }
+    Surpresa(int x, int y) : Itens("Surpresa", x, y) {}
+    void aplicarEfeito(Caravana* caravana) override;
 };
 
-#endif //TP_POO_ITENS_H
+#endif // TP_POO_ITENS_H
